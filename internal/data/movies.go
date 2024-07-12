363fa3c27af23756d6fcf7b18fa327c220da53c8
Sarath191181208 <vssarathc04@gmail.com>
2024-07-12 15:38:01 +0530

    json tags for data struct

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -3,11 +3,11 @@ package data
 import "time"
 
 type Movie struct {
-	ID        int64     // unique id of obj
-	CreatedAt time.Time // time at the movie is added to out db
-	Title     string    // title of the movie
-	Year      int32     // Movie release year
-	Runtime   int       // Movie runtime in minutes
-	Generes   []string  // slice of generes of the movie
-	Version   int32     // track number of updates to this particular movie record
+	ID        int64     `json:"id"`                       // unique id of obj
+	CreatedAt time.Time `json:"-"`                        // time at the movie is added to out db
+	Title     string    `json:"title"`                    // title of the movie
+	Year      int32     `json:"year,omitempty"`           // Movie release year
+	Runtime   int       `json:"runtime,omitempty,string"` // Movie runtime in minutes
+	Generes   []string  `json:"generes,omitempty"`        // slice of generes of the movie
+	Version   int32     `json:"version"`                  // track number of updates to this particular movie record
 }
