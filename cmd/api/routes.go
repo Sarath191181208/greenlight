0807923df3e5e594e6ad3f97d72c752914599bb1
Sarath191181208 <vssarathc04@gmail.com>
2024-07-12 13:08:48 +0530

    invalid / on :14 fixed

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -6,12 +6,12 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-func (app *Application) routes() *httprouter.Router{
-  router := httprouter.New()
+func (app *Application) routes() *httprouter.Router {
+	router := httprouter.New()
 
-  router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthCheckHandler)
-  router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
-  router.HandlerFunc(http.MethodGet, "v1/movies/:id", app.showMovieHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthCheckHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
 
-  return router
+	return router
 }
